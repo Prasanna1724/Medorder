eed28eab36e6f2a1b12879b2332ffe629315eebf
Prasanna1724 <124789544+Prasanna1724@users.noreply.github.com>
2024-11-04 12:40:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function ace(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function ibu(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function ant(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function rol(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function hyd(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function aml(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function amx(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function met(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function ins(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function gab(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function para(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function lev(medicineName) {
+    alert(`You have ordered: ${medicineName}`);
+}
+function place(){
+    alert('Your order has been placed successfully.!!!')
+}
+
+var mybutton = document.getElementById("upBtn");
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+      if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    function topFunction() {
+      document.body.scrollTop = 0; 
+      document.documentElement.scrollTop = 0; 
+    }
