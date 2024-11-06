85ebb578d3ac620d365c3feb5017df0c3f23e49d
Prasanna1724 <124789544+Prasanna1724@users.noreply.github.com>
2024-11-06 11:13:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 function ace(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function ibu(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function ant(medicineName) {
     alert(`You have ordered: ${medicineName}`);
 }
 function rol(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function hyd(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function aml(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function amx(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function met(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function ins(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function gab(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function para(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function lev(medicineName) {
-    alert(`You have ordered: ${medicineName}`);
+    alert("You have ordered: ${medicineName}");
 }
 function place(){
-    alert('Your order has been placed successfully.!!!')
+    alert("Your order has been placed successfully.!!!")
 }
 
 var mybutton = document.getElementById("upBtn");
